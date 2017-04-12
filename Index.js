28a07395c84a84e9f1a411be64073206e0467c82
tinabitna <kangt0627@gmail.com>
2017-04-11 21:09:30 -0400

    added file

---
+++ Index.js
@@ -0,0 +1,25 @@
+function add() { 
+	//console.log("let's add");
+	//var math = ;
+	console.log(1+1);
+}
+
+function subtract() { 
+	//console.log("let's subtract");
+	//var math = ;
+	console.log(1-1);
+}
+
+function multiply() { 
+	console.log("let's multiply");
+	//var math = ;
+	console.log(1*1);
+}
+
+function divide() { 
+	console.log("let's divide");
+	//console.log("let's divide");
+	//var math = ;
+	console.log(1/1);
+}
+
